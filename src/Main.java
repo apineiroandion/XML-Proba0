81eb0c742649bebd7482698aef9a315dba220c40
apineiroandion <apineiroandion@danielcastelao.org>
2024-10-24 14:34:07 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, XMLStreamException {
+        String ruta = "prueba.xml";
+        // Instanciamos XMLOutputFactory con newInstance porque es una clase abstracta
+        // y no se puede instanciar directamente.
+        XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        FileWriter fileWriter = new FileWriter(ruta);
+        XMLStreamWriter writer = factory.createXMLStreamWriter(fileWriter);
+        writer.writeStartDocument("1.0");
+            writer.writeStartElement("autores");
+                writer.writeStartElement("autor");
+                    writer.writeAttribute("codigo", "a1");
+                        writer.writeStartElement("nombre");
+                        writer.writeCharacters("Alexandre Dumas");
+                        writer.writeEndElement();
+
+                writer.writeEndElement();
+            writer.writeEndElement();
+
+    }
+}
